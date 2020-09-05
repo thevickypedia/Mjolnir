9bfd027ea70f33d94431029bf988b3e291f10d12
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-05 16:23:39 -0500

    reformat code

--- excalibur.py
+++ excalibur.py
@@ -1,10 +1,15 @@
 import requests
 
-username = 'thevickypedia'
-request = requests.get(f'https://api.github.com/users/{username}/repos')
-response = request.json()
 
-for i in range(len(response)):
-    print(response[i]['name'])
-    print(response[i]['svn_url'])
-    print()
+def list_repos():
+    request = requests.get(f'https://api.github.com/users/{username}/repos')
+    response = request.json()
+
+    for i in range(len(response)):
+        print(response[i]['name'])
+        # print(response[i]['svn_url'])
+
+
+if __name__ == '__main__':
+    username = input('Enter your GitHub username:\n')
+    list_repos()
